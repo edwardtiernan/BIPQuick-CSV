42dec1aeaea1e424c741846281c9d1dca668cad6
EddieT32 <41162996+EddieT32@users.noreply.github.com>
2019-09-03 14:03:05 -0500

    Files pertaining to the Optimal and Edge 1 Test Cases

---
+++ FileSettings.py
@@ -0,0 +1,13 @@
+
+global settingsdict, geneticdict
+
+#print("FileSettings found")
+
+
+settingsdict = {'nodes_info': "nodes_info_EDGE1.csv", 'links_info': "links_info_EDGE1.csv", 'multiprocessors': 300}
+
+
+#settingsdict = {'inputfilename': "SV_Moderate.inp", 'root': "O14", 'multiprocessors': 4}
+
+
+#settingsdict = {'inputfilename': "001_Brentwood_1D_Existing_Monitoring_rev1.inp", 'root': "BW1", 'multiprocessors': 20}
